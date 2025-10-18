e6ebcd54b4568bcecf7bcd9b191eaf424dc8399a
Mennaamohamed25 <Mennaa.mohamedd25@gmail.com>
2025-10-18 05:50:01 +0300

    Add root redirect page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,43 +1,15 @@
-// import { Lexend, Cairo } from "next/font/google";
-import { Inter } from "next/font/google";
+// src/app/layout.js
+import Navbar from '../components/common/Navbar.jsx';
+import Footer from '../components/common/Footer.jsx';
+import './globals.css';
 
-import "./globals.css";
-import Navbar from '../components/common/Navbar.jsx'
-import Footer from '../components/common/Footer.jsx'
-import { NextIntlClientProvider} from "next-intl"
-import { setRequestLocale } from "next-intl/server";
-import { hasLocale } from "next-intl";
-import { notFound } from "next/navigation";
-import { routing } from "../i18n/routing";
-
-const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-inter", 
-});
-
-
-
-export default async function LocaleLayout({ children, params }) {
-  const { locale } = await params;
-
-  if (!hasLocale(routing.locales, locale)) {
-    notFound();
-  }
-
-  setRequestLocale(locale);
-
-  const messages = (await import(`../../messages/${locale}.json`)).default;
-
+export default function RootLayout({ children }) {
   return (
-    <html     lang={locale}
-      dir={locale === "ar" ? "rtl" : "ltr"}>
+    <html lang="en">
       <body>
-        <NextIntlClientProvider messages={messages} locale={locale}>
-          <Navbar/>
-          {children}
-        <Footer/>
-        </NextIntlClientProvider>
+        <Navbar />
+        {children}
+        <Footer />
       </body>
     </html>
   );
